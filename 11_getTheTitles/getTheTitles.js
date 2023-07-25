fe2fee9915052359964df942b157b0a476ff1220
Kevan <44446640+kevankas@users.noreply.github.com>
2023-07-25 01:34:50 -0500

    finisheed all 12 exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let booking = [];
+    for (let i = 0; i < books.length; i++)
+    {
+        booking[i] = books[i].title;
+    }
 
+    return booking;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let placeholder = [];
+    for (let i = 0; i < people.length; i++)
+    {
+        placeholder[i] = people[i].yearOfDeath - people[i].yearOfBirth;
+        // console.log(placeholder[i]);
+
+
+        if (isNaN(placeholder[i]))
+      {
+        const d = new Date();
+        let year = d.getFullYear();
+        people[i].yearOfDeath = year; 
+        placeholder[i] = people[i].yearOfDeath - people[i].yearOfBirth;
+        // console.log(people[i])
+      }
+    }
+    
+    for (z = 0; z < people.length; z++)
+    {
+        if (placeholder[z] > placeholder[z+1])
+        {
+            // console.log(z);
+            return(people[z]);
+        }
+    }
+
 
 };
 
